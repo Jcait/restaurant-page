aad78fa33d86539913d18f6737b1b198ccd677b3
Jo <134626490+Jcait@users.noreply.github.com>
2024-05-22 14:56:32 +0100

    add click to buttons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import homeCards from './homeCards';
 import'./style.css';
 
 const con = document.getElementById("content")
+const btn = document.querySelectorAll("button")
+
+btn.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(`${button.innerText}`)
+    })
+})
 
 con.appendChild(home())
 con.appendChild(homeCards())
